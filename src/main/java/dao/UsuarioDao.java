d577d82bede40194bdaad8b680d76a88fc2ba100
Murilo0356 <murilorolim2005@gmail.com>
2025-12-08 17:56:24 -0300

    implementação da DAO usuario

---
+++ src/main/java/dao/UsuarioDao.java
@@ -0,0 +1,34 @@
+package dao;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import model.Usuario;
+
+public class UsuarioDao extends GenericDao<Usuario> {
+
+    public UsuarioDao() {
+        super(Usuario.class);
+    }
+
+    public List<Usuario> listarTodos() {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery("SELECT u FROM Usuario u", Usuario.class)
+                     .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+
+    public Usuario buscarPorEmail(String email) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.createQuery("SELECT u FROM Usuario u WHERE u.email = :email", Usuario.class)
+                     .setParameter("email", email)
+                     .getSingleResult();
+        } finally{
+            em.close();
+        }
+    }
+}
